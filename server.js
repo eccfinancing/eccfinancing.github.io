49331c0987c90113b9e2c687c4daed3dc189104a
omgimanerd <alvin.lin.dev@gmail.com>
2017-01-18 18:02:24 -0500

    temp

--- server.js
+++ server.js
@@ -34,59 +34,60 @@ app.use('/public', express.static(__dirname + '/public'));
 // Use request.body for POST request params.
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-  response.redirect('/index');
-});
-
-app.get('/:page', function(request, response, next) {
-  var pages = ['index', 'about', 'investment', 'portfolio', 'contact'];
-  if (pages.indexOf(request.params.page) >= 0) {
-    response.render(request.params.page);
-  } else {
-    next();
-  }
-});
-
-app.post('/message', function(request, response) {
-  /**
-   * The POST request must contain three fields:
-   * email - The sender email that we can reply to.
-   * name - The name of the person.
-   * message - The message content.
-   */
-  var name = (request.body.name || '').trim();
-  var email = (request.body.email || '').trim();
-  var ticker = (request.body.ticker || '').trim();
-  var message = (request.body.message || '').trim();
-  if (!name || !email || !message) {
-    response.send({
-      error: 'One of your message fields was blank!'
-    });
-  } else {
-    var alert1 = emailAlerts({
-      fromEmail: email,
-      toEmail: DEV_MODE ? process.env.DEV_EMAIL : process.env.TO_EMAIL_1,
-      apiKey: process.env.SENDGRID_API_KEY
-    });
-    var alert2 = emailAlerts({
-      fromEmail: email,
-      toEmail: DEV_MODE ? process.env.DEV_EMAIL : process.env.TO_EMAIL_2,
-      apiKey: process.env.SENDGRID_API_KEY
-    });
-    var subject = 'eccfinancing - Message from ' + request.body.name +
-    ' from ' + ticker;
-    alert1.alert(subject, request.body.message, function(error) {
-      alert2.alert(subject, request.body.message, function(error) {
-        response.send({
-          error: error
-        });
-      });
-    });
-  }
-});
+// app.get('/', function(request, response) {
+//    response.redirect('/index');
+// });
+//
+// app.get('/:page', function(request, response, next) {
+//   var pages = ['index', 'about', 'investment', 'portfolio', 'contact'];
+//   if (pages.indexOf(request.params.page) >= 0) {
+//     response.render(request.params.page);
+//   } else {
+//     next();
+//   }
+// });
+//
+// app.post('/message', function(request, response) {
+//   /**
+//    * The POST request must contain three fields:
+//    * email - The sender email that we can reply to.
+//    * name - The name of the person.
+//    * message - The message content.
+//    */
+//   var name = (request.body.name || '').trim();
+//   var email = (request.body.email || '').trim();
+//   var ticker = (request.body.ticker || '').trim();
+//   var message = (request.body.message || '').trim();
+//   if (!name || !email || !message) {
+//     response.send({
+//       error: 'One of your message fields was blank!'
+//     });
+//   } else {
+//     var alert1 = emailAlerts({
+//       fromEmail: email,
+//       toEmail: DEV_MODE ? process.env.DEV_EMAIL : process.env.TO_EMAIL_1,
+//       apiKey: process.env.SENDGRID_API_KEY
+//     });
+//     var alert2 = emailAlerts({
+//       fromEmail: email,
+//       toEmail: DEV_MODE ? process.env.DEV_EMAIL : process.env.TO_EMAIL_2,
+//       apiKey: process.env.SENDGRID_API_KEY
+//     });
+//     var subject = 'eccfinancing - Message from ' + request.body.name +
+//     ' from ' + ticker;
+//     alert1.alert(subject, request.body.message, function(error) {
+//       alert2.alert(subject, request.body.message, function(error) {
+//         response.send({
+//           error: error
+//         });
+//       });
+//     });
+//   }
+// });
 
 app.use(function(request, response) {
-  response.status(404).render('404');
+  response.send('Website under construction!');
+  // response.status(404).render('404');
 });
 
 // Starts the server.
